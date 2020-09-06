3fa44eb8b22c64d07b46f732bcd8e48618d32362
CohenArthur <arthur.cohen@epita.fr>
2020-09-06 15:40:27 +0200

    loop: impl Instruction for Loop

--- src/instruction/loop_block.rs
+++ src/instruction/loop_block.rs
@@ -1,7 +1,7 @@
 //! The Loop instruction is used for repeating instructions. They can be of three
 //! different kinds, `for`, `while` or `loop`.
 
-use super::{Block, Instruction, Var};
+use super::{Block, InstrKind, Instruction, Var};
 
 /// What kind of loop the loop block represents: Either a for Loop, with a variable and
 /// a range expression, a while loop with just an upper bound, or a loop with no bound
@@ -22,5 +22,26 @@ pub struct Loop {
 impl Loop {
     pub fn new(kind: LoopKind, block: Block) -> Loop {
         Loop { kind, block }
+    }
+}
+
+impl Instruction for Loop {
+    fn kind(&self) -> InstrKind {
+        self.block.kind()
+    }
+
+    fn print(&self) -> String {
+        match &self.kind {
+            LoopKind::For(var, range) => format!(
+                "for {} in {} {}",
+                var.name(),
+                range.print(),
+                self.block.print()
+            ),
+            LoopKind::While(condition) => {
+                format!("while {} {}", condition.print(), self.block.print())
+            }
+            LoopKind::Loop => format!("loop {}", self.block.print()),
+        }
     }
 }
