206f8fecb5090d4d9bcd77e1236b0da3ad8cadce
CohenArthur <arthur.cohen@epita.fr>
2020-09-11 18:01:23 +0200

    box_construct: Add box_construct! macro

--- src/parser/box_construct.rs
+++ src/parser/box_construct.rs
@@ -4,16 +4,28 @@
 
 use crate::instruction::Instruction;
 
+use super::constructs::Construct;
+
+macro_rules! box_construct {
+    ($func:ident) => {
+        pub fn $func(input: &str) -> nom::IResult<&str, Box<dyn Instruction>> {
+            BoxConstruct::new(input, Box::new(Construct::$func))
+        }
+    };
+}
+
 pub struct BoxConstruct;
 
 impl BoxConstruct {
     /// Call a `Construct` and box the return value
-    fn new<T: Instruction + 'static>(
-        input: &'static str,
-        construct: &dyn Fn(&str) -> nom::IResult<&str, T>,
-    ) -> nom::IResult<&'static str, Box<dyn Instruction>> {
+    fn new<T: 'static + Instruction>(
+        input: &str,
+        construct: Box<dyn FnOnce(&str) -> nom::IResult<&str, T>>,
+    ) -> nom::IResult<&str, Box<dyn Instruction>> {
         let (input, value) = construct(input)?;
 
         Ok((input, Box::new(value)))
     }
+
+    box_construct!{function_call}
 }
