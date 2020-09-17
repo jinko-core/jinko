de567033ee4cb811c19e65d654a5d12911ac22d5
Arthur Cohen <arthur.cohen@qarnot-computing.com>
2020-09-17 09:54:50 +0200

    parser: Add single-line comment consuming

--- src/parser/tokens.rs
+++ src/parser/tokens.rs
@@ -6,7 +6,7 @@ use nom::{
     bytes::complete::is_not, bytes::complete::tag, bytes::complete::take_until,
     bytes::complete::take_while, bytes::complete::take_while1, character::complete::anychar,
     character::complete::char, character::is_alphabetic, character::is_alphanumeric,
-    character::is_digit, combinator::opt, error::ErrorKind, sequence::delimited, IResult,
+    character::is_digit, combinator::opt, error::ErrorKind, sequence::delimited, IResult, branch::alt,
 };
 
 /// Reserved Keywords by jinko
@@ -236,11 +236,24 @@ impl Token {
         take_while(|c| Token::is_whitespace(c))(input)
     }
 
-    /// Consumes all kinds of comments: Multi-line or single-line
-    pub fn maybe_consume_comments(input: &str) -> IResult<&str, &str> {
+    fn maybe_consume_multi_comment(input: &str) -> IResult<&str, &str> {
         let (input, _) = Token::comment_multi_start(input)?;
         let (input, _) = take_until("*/")(input)?;
         Token::comment_multi_end(input)
+    }
+
+    fn maybe_consume_single_comment(input: &str) -> IResult<&str, &str> {
+        let (input, _) = Token::comment_single(input)?;
+        let (input, _) = take_while(|c| c != '\n' && c != '\0')(input)?;
+        match opt(char('\n'))(input) {
+            Ok((i, Some(_))) | Ok((i, None)) => Ok((i, "")),
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Consumes all kinds of comments: Multi-line or single-line
+    pub fn maybe_consume_comment(input: &str) -> IResult<&str, &str> {
+        alt((Token::maybe_consume_single_comment, Token::maybe_consume_multi_comment))(input)
     }
 }
 
@@ -362,27 +375,51 @@ mod tests {
 
     #[test]
     fn t_multi_comment_valid() {
-        match Token::maybe_consume_comments("/* */") {
+        match Token::maybe_consume_comment("/* */") {
             Ok(_) => assert!(true),
             Err(_) => assert!(false, "Valid to have one space"),
         };
-        match Token::maybe_consume_comments("/**/") {
+        match Token::maybe_consume_comment("/**/") {
             Ok(_) => assert!(true),
             Err(_) => assert!(false, "Valid to have zero space"),
         };
-        match Token::maybe_consume_comments("/*            */") {
+        match Token::maybe_consume_comment("/*            */") {
             Ok(_) => assert!(true),
             Err(_) => assert!(false, "Valid to have tons of spaces"),
         };
-        match Token::maybe_consume_comments("/* a bbbb a something   */") {
+        match Token::maybe_consume_comment("/* a bbbb a something   */") {
             Ok(_) => assert!(true),
             Err(_) => assert!(false, "Valid to have tons of text and stuff"),
         };
     }
 
     #[test]
+    fn t_single_comment_valid() {
+        match Token::maybe_consume_comment("//") {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false, "Valid to have nothing after the slashes"),
+        };
+        match Token::maybe_consume_comment("//                   ") {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false, "Valid to have lots of spaces"),
+        };
+        match Token::maybe_consume_comment("//          \nhey") {
+            Ok((left, _)) => assert_eq!(left, "hey"),
+            Err(_) => assert!(false, "Don't consume stuff after the newline"),
+        };
+        match Token::maybe_consume_comment("//// ") {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false, "Valid to have multiple slashes"),
+        };
+        match Token::maybe_consume_comment("// a bbbb a something  /* hey */") {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false, "Valid to have tons of text and stuff, even other comment"),
+        };
+    }
+
+    #[test]
     fn t_multi_comment_invalid() {
-        match Token::maybe_consume_comments("/*") {
+        match Token::maybe_consume_comment("/*") {
             Ok(_) => assert!(false, "Unclosed start delimiter"),
             Err(_) => assert!(true),
         };
