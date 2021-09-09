dd8802b20a94ce2afd9e3a70be230787b1f6beab
CohenArthur <arthur.cohen@epita.fr>
2021-09-09 23:29:46 +0200

    typechecker: Add type_cache macro

--- src/typechecker.rs
+++ src/typechecker.rs
@@ -1,22 +1,39 @@
 //! The [`TypeCheck`] trait enables a value to get resolved to a type.
 //! FIXME: MOAR DOC
 
-use crate::Context;
+use crate::{Context, instruction::TypeDec};
 
-pub enum CheckedType<T> {
-    Resolved(T),
+/// In order to avoid recomputing various types multiple times, instruction types should
+/// keep a [`CheckedType`] instance as a member function, which should be set and fetched
+/// using [`set_type()`] and [`get_type`]
+#[derive(Clone)]
+pub enum CheckedType {
+    Resolved(TypeDec),
     Void,
     Unknown,
 }
 
-pub trait TypeCheck<T> {
-    fn set_type(&mut self, ty: CheckedType<T>);
-    fn get_type(&self) -> CheckedType<&T>;
-    fn resolve_type(&self, ctx: &mut Context) -> CheckedType<T>;
+pub trait TypeCheck {
+    fn set_type(&mut self, ty: CheckedType);
+    fn get_type(&self) -> &CheckedType;
+    fn resolve_type(&self, ctx: &mut Context) -> CheckedType;
 
     fn type_check(&mut self, ctx: &mut Context) {
         if let CheckedType::Unknown = self.get_type() {
             self.set_type(self.resolve_type(ctx))
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! type_cache {
+    ($member:ident) => {
+        fn set_type(&mut self, ty: CheckedType) {
+            self.$member = ty
+        }
+
+        fn get_type(&self) -> &CheckedType {
+            &self.$member
         }
     }
 }
