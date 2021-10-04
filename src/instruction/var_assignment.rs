23958f225000e84d0d3a94fe9a4b4dc494a88763
CohenArthur <arthur.cohen@epita.fr>
2021-10-04 11:03:27 +0200

    var_assign: Implement typechecking

--- src/instruction/var_assignment.rs
+++ src/instruction/var_assignment.rs
@@ -1,7 +1,8 @@
 //! The VarAssign struct is used when assigning values to variables.
 
 use crate::instruction::{InstrKind, Var};
-use crate::{Context, ErrKind, Error, Instruction, ObjectInstance};
+use crate::typechecker::{CheckedType, TypeCtx};
+use crate::{Context, ErrKind, Error, Instruction, ObjectInstance, TypeCheck};
 
 #[derive(Clone)]
 pub struct VarAssign {
@@ -65,19 +66,7 @@ impl Instruction for VarAssign {
         let mut var_creation = false;
 
         let mut var = match ctx.get_variable(&self.symbol) {
-            Some(v) => {
-                // If `self` is mutable, then it means that we are creating the variable
-                // for the first time. However, we entered the match arm because the variable
-                // is already present in the context. Error out appropriately.
-                if self.mutable() {
-                    let err_msg =
-                        format!("trying to redefine already defined variable: {}", v.name());
-                    ctx.error(Error::new(ErrKind::Context).with_msg(err_msg));
-                    return None;
-                }
-
-                v.clone()
-            }
+            Some(v) => v.clone(),
             None => {
                 let mut new_v = Var::new(self.symbol().to_string());
                 new_v.set_mutable(self.mutable());
@@ -108,6 +97,59 @@ impl Instruction for VarAssign {
 
         // A variable assignment is always a statement
         None
+    }
+}
+
+impl TypeCheck for VarAssign {
+    fn resolve_type(&self, ctx: &mut TypeCtx) -> CheckedType {
+        let var_ty = match ctx.get_var(&self.symbol) {
+            // FIXME: Remove clone?
+            Some(checked_ty) => {
+                // If `self` is mutable, then it means that we are creating the variable
+                // for the first time. However, we entered the match arm because the variable
+                // is already present in the context. Error out appropriately.
+                if self.mutable() {
+                    let err_msg = format!(
+                        "trying to redefine already defined variable: {}",
+                        self.symbol()
+                    );
+                    ctx.error(Error::new(ErrKind::TypeChecker).with_msg(err_msg));
+                    return CheckedType::Unknown;
+                }
+
+                checked_ty.clone()
+            }
+            None => {
+                // FIXME: Add once trait bound
+                let instance_ty = CheckedType::Void; /* self.value.resolve_type(ctx); */
+                ctx.declare_var(self.symbol.clone(), instance_ty);
+
+                // We can return here since it's a new variable. This avoids checking
+                // the type later on
+                return CheckedType::Void;
+            }
+        };
+
+        // FIXME: We resolve value twice...
+        let value_ty = CheckedType::Void; /* self.value.resolve_type(ctx); */
+        if value_ty == CheckedType::Void {
+            ctx.error(Error::new(ErrKind::TypeChecker).with_msg(format!(
+                "trying to assign statement `{}` to variable `{}`",
+                self.value().print(),
+                self.symbol
+            )));
+            return CheckedType::Unknown;
+        }
+
+        if var_ty != value_ty {
+            ctx.error(Error::new(ErrKind::TypeChecker).with_msg(format!(
+                "trying to assign value of types `{}` to variable of type `{}`",
+                value_ty, var_ty
+            )));
+            return CheckedType::Unknown;
+        }
+
+        CheckedType::Void
     }
 }
 
@@ -172,7 +214,9 @@ mod tests {
         assert!(i.error_handler.has_errors());
     }
 
+    // Don't ignore once TypeCheck is a bound on Instruction
     #[test]
+    #[ignore]
     fn create_mutable_twice() {
         let mut i = Context::new();
         let va_init = Construct::mut_var_assignment("mut a = 13").unwrap().1;
