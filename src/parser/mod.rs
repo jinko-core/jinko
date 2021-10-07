cd5e349ad70b2af8665f94b27309b8eb1f2120e7
CohenArthur <arthur.cohen@epita.fr>
2021-10-07 12:29:04 +0200

    parser: Move macros outside of parser mod

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -20,28 +20,6 @@ pub type ParseResult<T, I> = nom::IResult<T, I, Error>;
 
 pub struct Parser;
 
-#[macro_export]
-macro_rules! jinko_ex {
-    ($($t:tt) *) => {
-        $crate::Parser::parse(stringify!( $( $t ) * )).unwrap().execute().unwrap();
-    }
-}
-
-#[macro_export]
-macro_rules! jinko {
-    ($($t:tt) *) => {
-        {
-            let mut ctx = Context::new();
-            let (_, insts) = $crate::parser::Construct::many_instructions(stringify!( $( $t ) * )).unwrap();
-            for inst in insts {
-                inst.execute(&mut ctx);
-            }
-
-            ctx
-        }
-    }
-}
-
 impl Parser {
     /// Parses the entire user input and returns a hashmap corresponding to the user
     /// program
