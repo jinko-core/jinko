471b995810ecb644abc8a7de573658f4ad936423
Arthur Cohen <arthur.cohen@embecosm.com>
2022-11-27 17:32:49 +0100

    flatten: Add base for flatten crate

---
+++ flatten/src/lib.rs
@@ -0,0 +1,52 @@
+use ast::{Ast, Node as AstNode};
+
+// FIXME: use fir::Fir;
+type Fir = ();
+
+pub trait FlattenAst: Sized {
+    fn flatten(self) -> Fir;
+}
+
+impl FlattenAst for ast::Ast {
+    fn flatten(self) -> Fir {
+        let Ast { location, node } = self;
+
+        match node {
+            AstNode::Block(_) => todo!(),
+            AstNode::Incl { source, as_path } => todo!(),
+            AstNode::Function {
+                kind,
+                name,
+                generics,
+                arguments,
+                return_type,
+                block,
+            } => todo!(),
+            AstNode::Type {
+                name,
+                generics,
+                fields,
+                with,
+            } => todo!(),
+            AstNode::TypeInstantiation(_) => todo!(),
+            AstNode::FunctionCall(_) => todo!(),
+            AstNode::MethodCall { instance, call } => todo!(),
+            AstNode::BinaryOp(_, _, _) => todo!(),
+            AstNode::FieldAccess(_, _) => todo!(),
+            AstNode::IfElse {
+                if_condition,
+                if_block,
+                else_block,
+            } => todo!(),
+            AstNode::VarAssign {
+                mutable,
+                to_assign,
+                value,
+            } => todo!(),
+            AstNode::Var(_) => todo!(),
+            AstNode::VarOrEmptyType(_) => todo!(),
+            AstNode::Loop(_, _) => todo!(),
+            AstNode::Return(_) => todo!(),
+        }
+    }
+}
