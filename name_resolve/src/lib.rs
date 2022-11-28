762b0be4ff31ce058b42189663929637714266ca
Arthur Cohen <arthur.cohen@embecosm.com>
2022-11-28 12:04:15 +0100

    name_resolver: Add basic one depth name resolver

--- name_resolve/src/lib.rs
+++ name_resolve/src/lib.rs
@@ -5,59 +5,121 @@ use symbol::Symbol;
 
 struct NameResolveCtx {
     current: OriginIdx,
-    _mappings: HashMap<Symbol, OriginIdx>,
+    mappings: HashMap<Symbol, OriginIdx>,
 }
 
-impl Pass for NameResolveCtx {
-    fn next_origin(&self) -> OriginIdx {
-        // FIXME: this is bugged. Needs to update self.current
-        self.current.next()
+impl NameResolveCtx {
+    fn insert_nodes(&mut self, fir: &Fir<Symbol>) {
+        fir.nodes.iter().for_each(|kv| {
+            let (origin, node) = kv;
+
+            if let Kind::FnDeclaration { .. } = &node.kind {
+                self.mappings.insert(node.data.clone(), *origin);
+            }
+        });
     }
 
-    fn pre_condition(_fir: &Fir) {}
+    fn resolve_symbol(&self, sym: &Symbol) -> RefIdx {
+        RefIdx::Resolved(*self.mappings.get(sym).unwrap())
+    }
+}
+
+impl Pass<Symbol> for NameResolveCtx {
+    fn next_origin(&mut self) -> OriginIdx {
+        // FIXME: this is bugged. Needs to update self.current
+        let old = self.current;
+        self.current = self.current.next();
+
+        old
+    }
+
+    fn pre_condition(_fir: &Fir<Symbol>) {}
 
     fn post_condition(_fir: &Fir) {}
 
-    fn transform(&mut self, fir: Fir) -> Fir {
-        let (def, call) = create_invalid_relationship();
+    fn transform(&mut self, fir: Fir<Symbol>) -> Fir {
+        self.insert_nodes(&fir);
 
-        fir.append(def).append(call)
+        fir.nodes.into_iter().fold(Fir::default(), |fir, kv| {
+            let (origin, node) = kv;
+
+            let resolved = self.resolve_symbol(&node.data);
+
+            let new_kind = match node.kind {
+                Kind::Call { generics, args, .. } => Kind::Call {
+                    to: resolved,
+                    generics,
+                    args,
+                },
+                // nothing to do for other types of nodes
+                kind => kind,
+            };
+
+            fir.append(Node {
+                data: (),
+                origin,
+                location: None,
+                kind: new_kind,
+            })
+        })
     }
 }
 
-fn create_invalid_relationship() -> (Node, Node) {
-    let origin = OriginIdx::default();
-    let call = origin.next();
-
-    (
-        Node {
-            origin,
-            location: None,
-            kind: Kind::Constant(RefIdx::Unresolved),
-        },
-        Node {
-            origin: call,
-            location: None,
-            kind: Kind::Call {
-                to: RefIdx::Resolved(origin),
-                generics: vec![],
-                args: vec![],
-            },
-        },
-    )
-}
-
 pub trait NameResolve {
-    fn name_resolve(self) -> Self;
+    fn name_resolve(self) -> Fir;
 }
 
-impl NameResolve for Fir {
-    fn name_resolve(self) -> Self {
+impl NameResolve for Fir<Symbol> {
+    #[must_use]
+    fn name_resolve(self) -> Fir {
         let mut ctx = NameResolveCtx {
             current: OriginIdx::default(),
-            _mappings: HashMap::new(),
+            mappings: HashMap::new(),
         };
 
         ctx.pass(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn name_resolve_simple() {
+        let fir = Fir::<Symbol> {
+            nodes: HashMap::new(),
+        }
+        // we declare a function "a"
+        .append(Node {
+            data: Symbol::from("a"),
+            origin: OriginIdx(0),
+            location: None,
+            kind: Kind::FnDeclaration {
+                generics: vec![],
+                args: vec![],
+            },
+        })
+        // we add a call to "a"
+        .append(Node {
+            data: Symbol::from("a"),
+            origin: OriginIdx(1),
+            location: None,
+            kind: Kind::Call {
+                to: RefIdx::Unresolved,
+                generics: vec![],
+                args: vec![],
+            },
+        });
+
+        let fir = fir.name_resolve();
+
+        let call = &fir.nodes[&OriginIdx(1)];
+
+        if let Kind::Call { to, .. } = call.kind {
+            assert_eq!(to, RefIdx::Resolved(OriginIdx(0)))
+        } else {
+            panic!()
+        }
     }
 }
--- name_resolve/src/main.rs
+++
@@ -1,7 +0,0 @@
-use fir::Fir;
-use name_resolve::NameResolve;
-
-fn main() {
-    let fir = Fir::default();
-    let _fir = fir.name_resolve();
-}
