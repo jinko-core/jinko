97dd5dab751f8c7c321d96c5bcc630fdd386ebdf
Arthur Cohen <arthur.cohen@embecosm.com>
2022-11-28 18:16:41 +0100

    fir: Do not depend on any other workspace crates

--- fir/src/lib.rs
+++ fir/src/lib.rs
@@ -95,11 +95,11 @@
 // Does that make sense? Does that indicate that for all types we must first keep a Option<Ty> which is set to None?
 // Is this going to cause problems?
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 
-use location::SpanTuple;
+mod checks;
 
 /// A reference to another [`Node`] in the [`Fir`]. These references can be either resolved or unresolved, based
 /// on the state of the [`Fir`].
@@ -145,50 +145,36 @@ impl<K: Hash + Eq, V> WithHashMap<K, V> for HashMap<K, V> {
     }
 }
 
-/// An abstraction over a statically typed value. This is similar to keeping a tuple of [`RefIdx`]s,
-/// but provides some helper methods
-#[derive(Debug, Clone)]
-pub struct TypedValue(RefIdx, RefIdx);
-
-impl TypedValue {
-    /// Get the underlying [`RefIdx`] to the "value" contained in the [`TypedValue`]
-    pub fn value(&self) -> RefIdx {
-        self.0
-    }
-
-    /// Get the underlying [`RefIdx`] to the type contained in the [`TypedValue`]
-    pub fn ty(&self) -> RefIdx {
-        self.1
-    }
-}
-
+// FIXME: Add proc-macro to generates the check automatically for each kind
 #[derive(Debug, Clone)]
 pub enum Kind {
     // FIXME: Figure out more leaf/basic block types
     Type {
         generics: Vec<RefIdx>, // to Kind::Generic
-        fields: Vec<TypedValue>,
+        fields: Vec<RefIdx>,   // to Kind::TypedValue
     },
     /// A statically typed constant. This can be either a literal or an empty type
     Constant(RefIdx), // to Kind::Type
-    // FIXME: Should we keep a TypedValue here as kind and instead of keeping
-    // Vec<TypedValue>, keep Vec<RefIdx> to Kind::TypedValue?
+    TypedValue {
+        value: RefIdx, // to Kind::{Call, Instantiation, TypedValue, Constant}
+        ty: RefIdx,    // to Kind::Type
+    },
     Generic {
-        default: Option<RefIdx>,
+        default: Option<RefIdx>, // to Kind::Type
     },
     FnDeclaration {
         generics: Vec<RefIdx>, // to Kind::Generic
-        args: Vec<TypedValue>,
+        args: Vec<RefIdx>,     // to Kind::TypedValue
     },
     Call {
         to: RefIdx,            // to Kind::FnDeclaration
         generics: Vec<RefIdx>, // to Kind::Type
-        args: Vec<TypedValue>,
+        args: Vec<RefIdx>,     // to Kind::TypedValue
     },
     Instantiation {
         to: RefIdx,            // to Kind::Type
         generics: Vec<RefIdx>, // to Kind::Type
-        fields: Vec<TypedValue>,
+        fields: Vec<RefIdx>,   // to Kind::TypedValue
     },
 }
 
@@ -196,11 +182,6 @@ pub enum Kind {
 pub struct Node<T: Debug = ()> {
     pub data: T,
     pub origin: OriginIdx,
-    // FIXME: Do we need to keep a Symbol here?
-    // FIXME: Can we avoid that by doing mappings between the AST and FIR?
-    // FIXME: Should we do something along the lines of Node<T = ()>? And keep an instance of T within the node?
-    // FIXME: Is that going to be annoying to use? Do we actually need a Location here?
-    pub location: Option<SpanTuple>,
     pub kind: Kind,
 }
 
@@ -216,100 +197,6 @@ impl<T: Debug> Fir<T> {
         Fir {
             nodes: self.nodes.with(node.origin, node),
         }
-    }
-
-    /// Check that all origins in the [`Fir`] are unique
-    ///
-    /// # Panic
-    ///
-    /// This function panic if the provided [`Fir`] contains an origin that is present multiple times.
-    fn check_unique_origins(&self) {
-        let mut origins = HashSet::new();
-        self.nodes.iter().for_each(|kv| {
-            let origin = kv.0;
-
-            match origins.get(origin) {
-                // FIXME: Do a nice error here
-                // FIXME: Add more info like both nodes having the same origin
-                // c\n\n{:#?}\n\nrefers to\n\n{:#?}\n\n[reference: {:?}]", $node, self.nodes[&origin], $ref),
-                Some(_) => panic!("non-unique `OriginIdx` detected in `Fir`"),
-                None => {
-                    origins.insert(origin);
-                }
-            }
-        })
-    }
-
-    /// Check if the [`Fir`] only contains links between entities allowed to link together.
-    /// For example, this asserts that there are no calls that have been resolved as calls to constant literals.
-    ///
-    /// # Panic
-    ///
-    /// This function panic if the provided [`Fir`] contains invalid relationships.
-    fn check_valid_links(&self) {
-        macro_rules! check {
-            ($ref:expr => $kind:pat, $node:expr) => {
-                if let RefIdx::Resolved(origin) = $ref {
-                    match self.nodes[&origin].kind {
-                        $kind => {}
-                        // FIXME: Do a nice error here
-                        _ => panic!("invalid relationship detected in `Fir`:\n\n{:#?}\n\nrefers to\n\n{:#?}\n\n[reference: {:?}]", $node, self.nodes[&origin], $ref),
-                    }
-                }
-            };
-            (@$iter:expr => $kind:pat, $node:expr) => {
-                $iter.iter().for_each(|value| check!(value => $kind, $node))
-            };
-        }
-
-        self.nodes.iter().for_each(|kv| {
-            let node = &kv.1;
-            match &node.kind {
-                Kind::Constant(r) => check!(r => Kind::Type { .. }, node),
-                Kind::Generic {
-                    default: Some(default),
-                } => check!(default => Kind::Type { .. }, node),
-                Kind::FnDeclaration {
-                    generics,
-                    args: _args,
-                } => {
-                    check!(@generics => Kind::Generic { .. }, node);
-                    // check!(@args => Kind::TypedValue);
-                }
-                Kind::Call {
-                    to,
-                    generics,
-                    args: _args,
-                } => {
-                    check!(to => Kind::FnDeclaration { .. }, node);
-                    check!(@generics => Kind::Type { .. }, node);
-                    // check!(@args => Kind::TypedValue);
-                }
-                Kind::Type {
-                    generics,
-                    fields: _fields,
-                } => {
-                    check!(@generics => Kind::Generic { .. }, node);
-                    // check!(@fields => Kind::TypedValue);
-                }
-                Kind::Instantiation {
-                    to,
-                    generics,
-                    fields: _fields,
-                } => {
-                    check!(to => Kind::Type { .. }, node);
-                    check!(@generics => Kind::Type { .. }, node);
-                    // check!(@fields => Kind::TypedValue);
-                }
-                // Nothing to do for generics without a default
-                Kind::Generic { .. } => {}
-            }
-        })
-    }
-
-    fn check(&self) {
-        self.check_unique_origins();
-        self.check_valid_links();
     }
 }
 
