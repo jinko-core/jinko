de25593af8b89abf9b2e8be0435fbfb9db0d142e
Arthur Cohen <arthur.cohen@embecosm.com>
2023-02-07 00:54:53 +0100

    loop_desugar: Add helper builder functions

--- loop_desugar/src/lib.rs
+++ loop_desugar/src/lib.rs
@@ -67,59 +67,80 @@ fn handle_loop_loop(loc: SpanTuple, block: Box<Ast>) -> Ast {
     }
 }
 
+fn call(to: &str, args: Vec<Ast>) -> Call {
+    Call {
+        to: Symbol::from(to),
+        generics: vec![],
+        args,
+    }
+}
+
+fn method(loc: &SpanTuple, instance: Box<Ast>, call: Call) -> Ast {
+    Ast {
+        location: loc.clone(),
+        node: Node::MethodCall { instance, call },
+    }
+}
+
+fn var_assign<S: Into<Symbol>>(loc: &SpanTuple, to_assign: S, value: Box<Ast>) -> Ast {
+    Ast {
+        location: loc.clone(),
+        node: Node::VarAssign {
+            mutable: false,
+            to_assign: to_assign.into(),
+            value,
+        },
+    }
+}
+
+fn block(loc: &SpanTuple, stmts: Vec<Ast>, last_is_expr: bool) -> Ast {
+    Ast {
+        location: loc.clone(),
+        node: Node::Block {
+            stmts,
+            last_is_expr,
+        },
+    }
+}
+
+fn var<S: Into<Symbol>>(loc: &SpanTuple, name: S) -> Ast {
+    Ast {
+        location: loc.clone(),
+        node: Node::Var(name.into()),
+    }
+}
+
 // TODO: Add macros to help generate AST fragments here
-fn handle_for_loop(loc: SpanTuple, iterator: Symbol, range: Box<Ast>, block: Box<Ast>) -> Ast {
+fn handle_for_loop(loc: SpanTuple, iterator: Symbol, range: Box<Ast>, loop_block: Box<Ast>) -> Ast {
     let range_var = "__#range";
-    let (stmts, last_is_expr) = match block.node {
+    let (stmts, last_is_expr) = match loop_block.node {
         Node::Block {
             stmts,
             last_is_expr,
         } => (stmts, last_is_expr),
-        _ => unreachable!("invalid AST: non-block expression in loop: {:?}", block),
+        _ => unreachable!(
+            "invalid AST: non-block expression in loop: {:?}",
+            loop_block
+        ),
     };
 
     fn inner(
-        loc: SpanTuple,
+        loc: &SpanTuple,
         iterator: Symbol,
         range: Box<Ast>,
         stmts: Vec<Ast>,
         last_is_expr: bool,
     ) -> Ast {
         // while <range>.has_next()
-        let range_has_next = Ast {
-            location: loc.clone(),
-            node: Node::MethodCall {
-                instance: range.clone(),
-                call: Call {
-                    to: Symbol::from("has_next"),
-                    ..Call::default()
-                },
-            },
-        };
+        let range_has_next = method(loc, range.clone(), call("has_next", vec![]));
         let kind = LoopKind::While(Box::new(range_has_next));
 
         // FIXME: Missing call to `unpack`
         // __#range.next()
-        let iter_next = Ast {
-            location: loc.clone(),
-            node: Node::MethodCall {
-                instance: range,
-                call: Call {
-                    to: Symbol::from("next"),
-                    ..Call::default()
-                },
-            },
-        };
+        let iter_next = method(loc, range, call("next", vec![]));
 
         // <iterator> = __#range.next();
-        let assignment = Ast {
-            location: loc.clone(),
-            node: Node::VarAssign {
-                mutable: false,
-                to_assign: iterator,
-                value: Box::new(iter_next),
-            },
-        };
+        let assignment = var_assign(loc, iterator, Box::new(iter_next));
 
         // {
         //     <iterator> = __#range.next();
@@ -127,43 +148,21 @@ fn handle_for_loop(loc: SpanTuple, iterator: Symbol, range: Box<Ast>, block: Box
         // }
         let mut new_stmts = vec![assignment];
         stmts.into_iter().for_each(|stmt| new_stmts.push(stmt));
-        let block = Ast {
-            location: loc.clone(),
-            node: Node::Block {
-                stmts: new_stmts,
-                last_is_expr,
-            },
-        };
+        let block = block(loc, new_stmts, last_is_expr);
 
         Ast {
-            location: loc,
+            location: loc.clone(),
             node: Node::Loop(kind, Box::new(block)),
         }
     }
 
     // __#range = <range>;
-    let range_create = Ast {
-        location: loc.clone(),
-        node: Node::VarAssign {
-            mutable: false,
-            to_assign: Symbol::from(range_var),
-            value: range,
-        },
-    };
+    let range_create = var_assign(&loc, range_var, range);
 
     // __#range
-    let new_range = Ast {
-        location: loc.clone(),
-        node: Node::Var(Symbol::from(range_var)),
-    };
+    let new_range = var(&loc, range_var);
 
-    let new_while = inner(
-        loc.clone(),
-        iterator,
-        Box::new(new_range),
-        stmts,
-        last_is_expr,
-    );
+    let new_while = inner(&loc, iterator, Box::new(new_range), stmts, last_is_expr);
 
     // {
     //     __#range = <range>;
