088c81fecc9c93aba69cf1d1da65fc946d3a5b67
Arthur Cohen <arthur.cohen@embecosm.com>
2023-02-07 22:17:17 +0100

    fir: Do not recursively visit nodes
    Since the Fir is a flat representation, all nodes are already visited

--- fir/src/lib.rs
+++ fir/src/lib.rs
@@ -132,6 +132,8 @@ impl OriginIdx {
     }
 }
 
+pub type Fallible<E> = Result<(), E>;
+
 /// Helper trait to enable an immutable pattern on the [`Fir`]
 trait WithHashMap<K: Hash + Eq, V> {
     fn with(self, key: K, value: V) -> Self;
@@ -280,128 +282,142 @@ pub trait VisitError: Sized {
 
 /// All of the helpers and visitors call back into [`visit_node`]
 pub trait Visitor<T: Debug, E: VisitError> {
-    fn visit_constant(&mut self, fir: &Fir<T>, constant: &RefIdx) -> Result<(), E> {
-        self.reference(fir, constant)
+    fn visit_constant(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _constant: &RefIdx,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_type_reference(&mut self, fir: &Fir<T>, reference: &RefIdx) -> Result<(), E> {
-        self.reference(fir, reference)
+    fn visit_type_reference(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _reference: &RefIdx,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_typed_value(&mut self, fir: &Fir<T>, value: &RefIdx, ty: &RefIdx) -> Result<(), E> {
-        self.reference(fir, value)?;
-        self.reference(fir, ty)
+    fn visit_typed_value(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _value: &RefIdx,
+        _ty: &RefIdx,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_generic(&mut self, fir: &Fir<T>, default: &Option<RefIdx>) -> Result<(), E> {
-        self.opt(fir, default)
+    fn visit_generic(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _default: &Option<RefIdx>,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
     fn visit_type(
         &mut self,
-        fir: &Fir<T>,
-        generics: &[RefIdx],
-        fields: &[RefIdx],
-    ) -> Result<(), E> {
-        self.fold(fir, generics)?;
-        self.fold(fir, fields)
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _generics: &[RefIdx],
+        _fields: &[RefIdx],
+    ) -> Fallible<E> {
+        Ok(())
     }
 
     fn visit_function(
         &mut self,
-        fir: &Fir<T>,
-        generics: &[RefIdx],
-        args: &[RefIdx],
-        return_ty: &Option<RefIdx>,
-        block: &Option<RefIdx>,
-    ) -> Result<(), E> {
-        self.fold(fir, generics)?;
-        self.fold(fir, args)?;
-        self.opt(fir, return_ty)?;
-        self.opt(fir, block)
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _generics: &[RefIdx],
+        _args: &[RefIdx],
+        _return_ty: &Option<RefIdx>,
+        _block: &Option<RefIdx>,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
     fn visit_instantiation(
         &mut self,
-        fir: &Fir<T>,
-        to: &RefIdx,
-        generics: &[RefIdx],
-        fields: &[RefIdx],
-    ) -> Result<(), E> {
-        self.reference(fir, to)?;
-        self.fold(fir, generics)?;
-        self.fold(fir, fields)
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _to: &RefIdx,
+        _generics: &[RefIdx],
+        _fields: &[RefIdx],
+    ) -> Fallible<E> {
+        Ok(())
     }
 
     fn visit_call(
         &mut self,
-        fir: &Fir<T>,
-        to: &RefIdx,
-        generics: &[RefIdx],
-        args: &[RefIdx],
-    ) -> Result<(), E> {
-        self.reference(fir, to)?;
-        self.fold(fir, generics)?;
-        self.fold(fir, args)
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _to: &RefIdx,
+        _generics: &[RefIdx],
+        _args: &[RefIdx],
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_statements(&mut self, fir: &Fir<T>, stmts: &[RefIdx]) -> Result<(), E> {
-        self.fold(fir, stmts)
+    fn visit_statements(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _stmts: &[RefIdx],
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_return(&mut self, fir: &Fir<T>, expr: &Option<RefIdx>) -> Result<(), E> {
-        self.opt(fir, expr)
+    fn visit_return(
+        &mut self,
+        _fir: &Fir<T>,
+        _node: &Node<T>,
+        _expr: &Option<RefIdx>,
+    ) -> Fallible<E> {
+        Ok(())
     }
 
-    fn visit_node(&mut self, fir: &Fir<T>, node: &Node<T>) -> Result<(), E> {
+    fn visit_node(&mut self, fir: &Fir<T>, node: &Node<T>) -> Fallible<E> {
         match &node.kind {
-            Kind::Constant(c) => self.visit_constant(fir, c),
-            Kind::TypeReference(r) => self.visit_type_reference(fir, r),
-            Kind::TypedValue { value, ty } => self.visit_typed_value(fir, value, ty),
-            Kind::Generic { default } => self.visit_generic(fir, default),
-            Kind::Type { generics, fields } => self.visit_type(fir, generics, fields),
+            Kind::Constant(c) => self.visit_constant(fir, node, c),
+            Kind::TypeReference(r) => self.visit_type_reference(fir, node, r),
+            Kind::TypedValue { value, ty } => self.visit_typed_value(fir, node, value, ty),
+            Kind::Generic { default } => self.visit_generic(fir, node, default),
+            Kind::Type { generics, fields } => self.visit_type(fir, node, generics, fields),
             Kind::Function {
                 generics,
                 args,
                 return_type,
                 block,
-            } => self.visit_function(fir, generics, args, return_type, block),
+            } => self.visit_function(fir, node, generics, args, return_type, block),
             Kind::Instantiation {
                 to,
                 generics,
                 fields,
-            } => self.visit_instantiation(fir, to, generics, fields),
-            Kind::Call { to, generics, args } => self.visit_call(fir, to, generics, args),
-            Kind::Statements(stmts) => self.visit_statements(fir, stmts),
-            Kind::Return(expr) => self.visit_return(fir, expr),
+            } => self.visit_instantiation(fir, node, to, generics, fields),
+            Kind::Call { to, generics, args } => self.visit_call(fir, node, to, generics, args),
+            Kind::Statements(stmts) => self.visit_statements(fir, node, stmts),
+            Kind::Return(expr) => self.visit_return(fir, node, expr),
         }
     }
 
-    fn reference(&mut self, fir: &Fir<T>, idx: &RefIdx) -> Result<(), E> {
-        match idx {
-            RefIdx::Unresolved => Err(E::simple()),
-            RefIdx::Resolved(origin) => self.visit_node(fir, &fir.nodes[origin]),
-        }
-    }
-
-    fn opt(&mut self, fir: &Fir<T>, idx: &Option<RefIdx>) -> Result<(), E> {
-        match idx {
-            Some(idx) => self.reference(fir, idx),
-            None => Ok(()),
-        }
-    }
-
-    fn fold(&mut self, fir: &Fir<T>, indexes: &[RefIdx]) -> Result<(), E> {
-        let errs =
-            indexes
-                .iter()
-                .fold(Vec::new(), |mut errs, idx| match self.reference(fir, idx) {
+    fn visit(&mut self, fir: &Fir<T>) -> Fallible<E> {
+        let errs = fir
+            .nodes
+            .values()
+            .fold(Vec::new(), |mut errs: Vec<E>, node| {
+                match self.visit_node(fir, node) {
                     Ok(_) => errs,
                     Err(e) => {
                         errs.push(e);
                         errs
                     }
-                });
+                }
+            });
 
         if errs.is_empty() {
             Ok(())
