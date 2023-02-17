7293bc1ac7fcbab97b06e93313325a40283691f1
Arthur Cohen <arthur.cohen@embecosm.com>
2023-02-17 22:35:23 +0100

    fir: Use BTreeMap instead, add Incomplete type

--- fir/src/iter.rs
+++ fir/src/iter.rs
@@ -6,8 +6,13 @@ pub use mapper::Mapper;
 pub use multi_mapper::MultiMapper;
 pub use traverse::Traversal;
 
+use crate::Fir;
+
 // FIXME: Documentation
 pub trait IterError: Sized {
     // FIXME: Documentation
     fn aggregate(errs: Vec<Self>) -> Self;
 }
+
+// FIXME: Documentation
+pub struct Incomplete<T, E>(pub Fir<T>, pub E);
