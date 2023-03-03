c29fea2ba14e2d7772215f870b4179e9fee8b6b1
Arthur Cohen <arthur.cohen@embecosm.com>
2023-03-03 22:47:21 +0100

    wip: Add more to interpolation parser

--- interpolate/src/lib.rs
+++ interpolate/src/lib.rs
@@ -1,48 +1,98 @@
 use ast::{Ast, Node, Value, Visitor};
 use error::{ErrKind, Error};
-use location::{Source, SpanTuple};
-use xparser::{ParseInput, ParseResult};
+use location::{Location, Source, SpanTuple};
+use nom::multi::many0;
+use xparser::{Error as ParseError, ParseInput, ParseResult};
 
-use nom::bytes::complete::take_until;
 use nom::character::complete::char;
+use nom::{bytes::complete::take_until, combinator::opt};
 use nom_locate::{position, LocatedSpan};
 
 struct Ctx;
 
-fn parse_expr(to_parse: ParseInput) -> ParseResult<ParseInput, Ast> {
-    let (input, _) = char('{')(to_parse)?;
+fn parse_expr(input: ParseInput) -> ParseResult<ParseInput, Ast> {
+    let (input, _) = char('{')(input)?;
     let (input, expr) = xparser::expr(input)?;
     let (input, _) = char('}')(input)?;
 
     Ok((input, expr))
 }
 
-fn parse_str(to_parse: ParseInput) -> ParseResult<ParseInput, ParseInput> {
+fn parse_str(input: ParseInput) -> ParseResult<ParseInput, ParseInput> {
     // how to handle reaching the end of the string?
-    take_until("{")(to_parse)
+    take_until("{")(input)
 }
 
-fn parse_inner(to_parse: &str) -> ParseResult<ParseInput, Ast> {
-    todo!()
+pub(crate) fn pos_to_loc(
+    input: ParseInput,
+    start: impl Into<Location>,
+    end: impl Into<Location>,
+) -> SpanTuple {
+    SpanTuple::with_source_ref(input.extra, start.into(), end.into())
+}
+
+fn parse_inner(input: ParseInput) -> ParseResult<ParseInput, Ast> {
+    let (input, start) = position(input)?;
+    let (input, s) = opt(parse_str)(input)?;
+    let (input, end) = position(input)?;
+    if let Some(s) = s {
+        return Ok((
+            input,
+            Ast {
+                location: pos_to_loc(input, start, end),
+                node: Node::Constant(Value::Str(s.fragment().to_string())),
+            },
+        ));
+    }
+
+    let (input, s) = opt(parse_expr)(input)?;
+    let (input, end) = position(input)?;
+    if let Some(expr) = s {
+        return Ok((input, expr));
+    }
+
+    let loc = pos_to_loc(input, start, end);
+
+    Error::new(ErrKind::Parsing)
+        .with_msg(format!("unexpected character in format string: {input}"))
+        .with_loc(Some(loc))
+        .emit();
+
+    Err(nom::Err::Error(ParseError))
 }
 
 /// This function parses the following grammar:  `double_quote ( .* ( '{' expr '}' )* )* double_quote`
 fn parse_format_string(to_parse: &str) -> Result<Vec<Ast>, Error> {
     let input = LocatedSpan::new_extra(to_parse, Source::Input(to_parse));
-    todo!()
+
+    let res = many0(parse_inner)(input);
+
+    if let Ok((_, exprs)) = res {
+        Ok(exprs)
+    } else {
+        Err(Error::new(ErrKind::Parsing))
+    }
 }
 
 impl Visitor for Ctx {
     fn visit_constant(&mut self, location: SpanTuple, value: Value) -> Result<Ast, Error> {
         let s = match value {
             Value::Str(s) => s,
-            _ => todo!(),
+            any_const => {
+                return Ok(Ast {
+                    location,
+                    node: Node::Constant(any_const),
+                })
+            }
         };
 
         Error::new(ErrKind::Hint)
             .with_loc(Some(location.clone()))
             .with_msg(format!("saw a string: {s}!"))
             .emit_debug();
+
+        let exprs = parse_format_string(&s)?;
+        dbg!(exprs);
 
         Ok(Ast {
             location,
