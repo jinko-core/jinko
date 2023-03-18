b7225f1cdea4863d2fe35d624c67cc6a7db79417
Arthur Cohen <arthur.cohen@embecosm.com>
2023-03-18 14:38:49 +0100

    typecheck: Add Actual and Checker passes
    This completes a first iteration of the type checker which works for
    simple cases

--- typecheck/src/actual.rs
+++ typecheck/src/actual.rs
@@ -1,5 +1,5 @@
 use error::Error;
-use fir::{Fallible, Fir, OriginIdx, RefIdx, Traversal};
+use fir::{Fallible, Fir, Kind, Node, OriginIdx, RefIdx, Traversal};
 use flatten::FlattenData;
 
 use crate::{Type, TypeCtx};
@@ -7,21 +7,66 @@ use crate::{Type, TypeCtx};
 /// This pass takes care of shortening each link within the type context as much as possible. As explained in
 /// the previous module ([`crate::typer`]), a list will be built within the [`TypeCtx`] containing "type links".
 /// This pass will resolve these links and make each node point to its *actual* type.
-pub(crate) struct Actual(pub(crate) TypeCtx);
+pub(crate) struct Actual<'ctx>(pub(crate) &'ctx mut TypeCtx);
 
-impl Actual {
-    /// Recursively try and resolve a type link within the type context
-    fn resolve_link(&mut self, fir: &Fir<FlattenData>, to_resolve: &OriginIdx) -> Fallible<Error> {
+fn innermost_type(fir: &Fir<FlattenData>, linked_node: RefIdx) -> Option<Type> {
+    let linked_node = match linked_node {
+        RefIdx::Unresolved => unreachable!(),
+        RefIdx::Resolved(r) => r,
+    };
+    let linked_node = &fir.nodes[&linked_node];
+
+    let inner_opt = |fir, opt| match opt {
+        Some(opt) => innermost_type(fir, opt),
+        None => None,
+    };
+
+    match &linked_node.kind {
+        // these are the *only* terminal branches
+        Kind::Type { .. } => Some(Type::One(RefIdx::Resolved(linked_node.origin))),
+        Kind::Assignment { .. } => None,
+        Kind::Constant(ty)
+        | Kind::TypeReference(ty)
+        | Kind::TypedValue { ty, .. }
+        | Kind::Binding { to: ty }
+        | Kind::Instantiation { to: ty, .. }
+        | Kind::Call { to: ty, .. }
+        | Kind::Conditional { true_block: ty, .. } => innermost_type(fir, *ty),
+        Kind::Function {
+            return_type: ty, ..
+        }
+        | Kind::Return(ty) => inner_opt(fir, *ty),
+        Kind::Statements(stmts) => inner_opt(fir, stmts.last().copied()),
+        // FIXME: What to do here?
+        Kind::Generic { .. } => todo!(),
+        Kind::Loop { .. } => todo!(),
+        Kind::TypeOffset { .. } => todo!(),
+    }
+}
+
+impl<'ctx> Actual<'ctx> {
+    /// Recursively try and resolve a type link within the type context. This will update the given node's type
+    /// within the type context.
+    fn resolve_link(&mut self, fir: &Fir<FlattenData>, to_resolve: OriginIdx) -> Fallible<Error> {
         // if any node has not been through "Typer" before, this is an interpreter error
-        let link = self.0.types.get(to_resolve).unwrap();
+        let link = self.0.types.get(&to_resolve).unwrap();
 
         // void nodes are already fully typed, so we don't take care of them
-        if let Type::One(ty_ref) = link {
-            let link_node = fir.nodes[ty_ref];
+        if let Some(Type::One(ty_ref)) = link {
+            let innermost_ty = innermost_type(fir, *ty_ref);
+            self.0.types.insert(to_resolve, innermost_ty).unwrap();
         }
 
         Ok(())
     }
 }
 
-impl Traversal<FlattenData, Error> for Actual {}
+impl<'ctx> Traversal<FlattenData, Error> for Actual<'ctx> {
+    fn traverse_node(
+        &mut self,
+        fir: &Fir<FlattenData>,
+        node: &Node<FlattenData>,
+    ) -> Fallible<Error> {
+        self.resolve_link(fir, node.origin)
+    }
+}
--- typecheck/src/checker.rs
+++ typecheck/src/checker.rs
@@ -1,1 +1,75 @@
+//! Finally, after all nodes have been typed and simplified, we make sure that types are
+//! valid. This includes checking that a function and its block return the same type, that
+//! both branches of a condition return the same type, etc
 
+use error::{ErrKind, Error};
+use fir::{Fallible, Fir, Node, RefIdx, Traversal};
+use flatten::FlattenData;
+use location::SpanTuple;
+use symbol::Symbol;
+
+use crate::{Type, TypeCtx};
+
+// TODO: We need a way to fetch common type nodes used during typechecking, such as `bool` for example,
+// as it's used for conditions for multiple nodes
+pub(crate) struct Checker<'ctx>(pub(crate) &'ctx mut TypeCtx);
+
+impl<'ctx> Checker<'ctx> {
+    fn get_type(&self, of: &RefIdx) -> Option<Type> {
+        match of {
+            RefIdx::Resolved(of) => *self.0.types.get(of).unwrap(),
+            RefIdx::Unresolved => unreachable!("unresolved node in `Fir`"),
+        }
+    }
+}
+
+fn type_mismatch(
+    loc: &Option<SpanTuple>,
+    fir: &Fir<FlattenData>,
+    expected: Option<Type>,
+    got: Option<Type>,
+) -> Error {
+    let void = Symbol::from("void");
+    let get_symbol = |ty| match ty {
+        Type::One(RefIdx::Resolved(idx)) => fir.nodes[&idx].data.symbol.clone().unwrap(),
+        _ => unreachable!("unresolved pattern"),
+    };
+
+    let expected_ty = expected.map_or(void.clone(), get_symbol);
+    let got_ty = got.map_or(void, get_symbol);
+
+    Error::new(ErrKind::TypeChecker)
+        .with_msg(format!(
+            "type mismatch found: expected `{expected_ty}`, got `{got_ty}`"
+        ))
+        .with_loc(loc.clone()) // FIXME: Missing hint
+}
+
+impl<'ctx> Traversal<FlattenData, Error> for Checker<'ctx> {
+    fn traverse_function(
+        &mut self,
+        fir: &Fir<FlattenData>,
+        node: &Node<FlattenData>,
+        _generics: &[RefIdx],
+        _args: &[RefIdx],
+        return_ty: &Option<RefIdx>,
+        block: &Option<RefIdx>,
+    ) -> Fallible<Error> {
+        let ret_ty = return_ty.as_ref().and_then(|b| self.get_type(b));
+        let block_ty = block.as_ref().and_then(|b| self.get_type(b));
+
+        match (ret_ty, block_ty) {
+            // All functions which do not have a return type are by definition
+            // typed correctly
+            (_, None) => Ok(()),
+            // if a function should return void, but returns
+            (ret_ty, block_ty) => {
+                if ret_ty != block_ty {
+                    Err(type_mismatch(&node.data.location, fir, ret_ty, block_ty))
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }
+}
