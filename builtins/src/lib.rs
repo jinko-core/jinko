765ea3d35ad2500b188eaffc99e65a4386ee4a4e
Arthur Cohen <arthur.cohen@embecosm.com>
2023-10-29 12:27:11 +0100

    builtins: Improve API

--- builtins/src/lib.rs
+++ builtins/src/lib.rs
@@ -1,8 +1,35 @@
 use ast::{Ast, Node};
 use error::Error;
 
+use std::slice;
+
+mod builder;
+
 pub const TYPE_COMPARABLE: &str = "builtin.comparable";
 pub const TYPE_NUMBER: &str = "builtin.number";
+
+macro_rules! name {
+    ($name:tt -> $str:literal) => {
+        pub const $name: &str = $str;
+    };
+}
+
+mod name {
+    name![ADD -> "+"];
+    name![SUB -> "-"];
+    name![MUL -> "*"];
+    name![DIV -> "/"];
+
+    name![EQ -> "=="];
+    name![NE -> "!="];
+    name![LT -> "<"];
+    name![LTE -> "<="];
+    name![GT -> ">"];
+    name![GTE -> ">="];
+
+    name![NOT -> "!unary"];
+    name![MIN -> "-unary"];
+}
 
 /// This enum does not match the actual primitive types of `jinko` on purpose, as builtin operators
 /// are a little different. For example, we care about whether or not a builtin is implemented on *numbers*,
@@ -24,27 +51,150 @@ impl BuiltinType {
     }
 }
 
-// Do we need some type information here? probably, right?
-pub const ARITHMETIC: &[(&str, BuiltinType)] = &[
-    ("+", BuiltinType::Number),
-    ("-", BuiltinType::Number),
-    ("*", BuiltinType::Number),
-    ("/", BuiltinType::Number),
-];
-pub const COMPARISON: &[(&str, BuiltinType)] = &[
-    ("==", BuiltinType::Comparable),
-    ("=!", BuiltinType::Comparable),
-    ("<=", BuiltinType::Comparable),
-    (">=", BuiltinType::Comparable),
-    ("<", BuiltinType::Comparable),
-    (">", BuiltinType::Comparable),
-];
-pub const UNARY: &[(&str, BuiltinType)] = &[
-    ("!unary", BuiltinType::Bool),
-    ("-unary", BuiltinType::Number),
-];
+pub enum Arithmetic {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
 
-mod builder;
+impl Arithmetic {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Arithmetic::Add => name::ADD,
+            Arithmetic::Sub => name::SUB,
+            Arithmetic::Mul => name::MUL,
+            Arithmetic::Div => name::DIV,
+        }
+    }
+
+    pub fn iter() -> slice::Iter<'static, Arithmetic> {
+        static VALUES: &[Arithmetic] = &[
+            Arithmetic::Add,
+            Arithmetic::Sub,
+            Arithmetic::Mul,
+            Arithmetic::Div,
+        ];
+
+        VALUES.iter()
+    }
+}
+
+pub enum Mode {
+    OrEqual,
+    Strict,
+}
+
+pub enum Comparison {
+    Equals,
+    Differs,
+    LessThan(Mode),
+    GreaterThan(Mode),
+}
+
+impl Comparison {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Comparison::Equals => name::EQ,
+            Comparison::Differs => name::NE,
+            Comparison::LessThan(Mode::Strict) => name::LT,
+            Comparison::LessThan(Mode::OrEqual) => name::LTE,
+            Comparison::GreaterThan(Mode::Strict) => name::GT,
+            Comparison::GreaterThan(Mode::OrEqual) => name::GTE,
+        }
+    }
+
+    pub fn iter() -> slice::Iter<'static, Comparison> {
+        static VALUES: &[Comparison] = &[
+            Comparison::Equals,
+            Comparison::Differs,
+            Comparison::LessThan(Mode::Strict),
+            Comparison::LessThan(Mode::OrEqual),
+            Comparison::GreaterThan(Mode::Strict),
+            Comparison::GreaterThan(Mode::OrEqual),
+        ];
+
+        VALUES.iter()
+    }
+}
+
+pub enum Unary {
+    Not,
+    Minus,
+}
+
+impl Unary {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Unary::Not => name::NOT,
+            Unary::Minus => name::MIN,
+        }
+    }
+
+    pub fn ty(&self) -> BuiltinType {
+        match self {
+            Unary::Not => BuiltinType::Bool,
+            Unary::Minus => BuiltinType::Number,
+        }
+    }
+
+    pub fn iter() -> slice::Iter<'static, Unary> {
+        static VALUES: &[Unary] = &[Unary::Not, Unary::Minus];
+
+        VALUES.iter()
+    }
+}
+
+pub enum Operator {
+    Arithmetic(Arithmetic),
+    Comparison(Comparison),
+    Unary(Unary),
+}
+
+impl Operator {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Operator::Arithmetic(inner) => inner.as_str(),
+            Operator::Comparison(inner) => inner.as_str(),
+            Operator::Unary(inner) => inner.as_str(),
+        }
+    }
+
+    pub fn ty(&self) -> BuiltinType {
+        match self {
+            Operator::Arithmetic(_) => BuiltinType::Number,
+            Operator::Comparison(_) => BuiltinType::Comparable,
+            Operator::Unary(Unary::Not) => BuiltinType::Bool,
+            Operator::Unary(Unary::Minus) => BuiltinType::Number,
+        }
+    }
+
+    pub fn try_from_str(s: &str) -> Option<Operator> {
+        use Arithmetic::*;
+        use Comparison::*;
+        use Unary::*;
+
+        use Operator as Op;
+
+        match s {
+            name::ADD => Some(Op::Arithmetic(Add)),
+            name::SUB => Some(Op::Arithmetic(Sub)),
+            name::MUL => Some(Op::Arithmetic(Mul)),
+            name::DIV => Some(Op::Arithmetic(Div)),
+
+            name::EQ => Some(Op::Comparison(Equals)),
+            name::NE => Some(Op::Comparison(Differs)),
+            name::LT => Some(Op::Comparison(LessThan(Mode::Strict))),
+            name::LTE => Some(Op::Comparison(LessThan(Mode::OrEqual))),
+            name::GT => Some(Op::Comparison(GreaterThan(Mode::Strict))),
+            name::GTE => Some(Op::Comparison(GreaterThan(Mode::OrEqual))),
+
+            name::NOT => Some(Op::Unary(Not)),
+            name::MIN => Some(Op::Unary(Minus)),
+            _ => None,
+        }
+    }
+}
 
 pub trait AppendAstBuiltins: Sized {
     fn append_builtins(self) -> Result<Self, Error>;
@@ -67,36 +217,36 @@ impl AppendAstBuiltins for Ast {
 
         // this creates a list of functions named "+", "-", "*", etc which we
         // can then add to the nodes of our AST.
-        let arithmetic_builtins = ARITHMETIC.iter().map(|(sym, ty)| {
+        let arithmetic_builtins = Arithmetic::iter().map(|op| {
             builder::function(
-                sym,
+                op.as_str(),
                 vec![
                     // FIXME: Use something else than `ty("int")` here. but what?
-                    builder::argument("lhs", builder::ty_arg(*ty)),
-                    builder::argument("rhs", builder::ty_arg(*ty)),
+                    builder::argument("lhs", builder::ty_arg(BuiltinType::Number)),
+                    builder::argument("rhs", builder::ty_arg(BuiltinType::Number)),
                 ],
-                Some(builder::ty_arg(*ty)),
+                Some(builder::ty_arg(BuiltinType::Number)),
             )
         });
 
-        let cmp_builtins = COMPARISON.iter().map(|(sym, ty)| {
+        let cmp_builtins = Comparison::iter().map(|op| {
             builder::function(
-                sym,
+                op.as_str(),
                 vec![
                     // FIXME: Use something else than `ty("int")` here. but what?
-                    builder::argument("lhs", builder::ty_arg(*ty)),
-                    builder::argument("rhs", builder::ty_arg(*ty)),
+                    builder::argument("lhs", builder::ty_arg(BuiltinType::Comparable)),
+                    builder::argument("rhs", builder::ty_arg(BuiltinType::Comparable)),
                 ],
                 Some(builder::ty_arg(BuiltinType::Bool)),
             )
         });
 
         // unary operators, like !bool or !int
-        let unary_builtins = UNARY.iter().map(|(sym, ty)| {
+        let unary_builtins = Unary::iter().map(|op| {
             builder::function(
-                sym,
-                vec![builder::argument("value", builder::ty_arg(*ty))],
-                Some(builder::ty_arg(*ty)),
+                op.as_str(),
+                vec![builder::argument("value", builder::ty_arg(op.ty()))],
+                Some(builder::ty_arg(op.ty())),
             )
         });
 
