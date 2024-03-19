90564f48a41b7500c9f5ca5a40fd86b40e8d0102
Arthur Cohen <arthur.cohen@embecosm.com>
2024-03-20 00:03:31 +0100

    fir: Fix unused warning

--- fir/src/iter/traverse.rs
+++ fir/src/iter/traverse.rs
@@ -76,7 +76,7 @@ pub trait Traversal<T, E> {
         _fir: &Fir<T>,
         _node: &Node<T>,
         _to: &Option<RefIdx>,
-        ty: &Option<RefIdx>,
+        _ty: &Option<RefIdx>,
     ) -> Fallible<E> {
         Ok(())
     }
