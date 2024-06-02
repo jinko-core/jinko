e2e634c958a2edc623ebb6a9cf2282974253ff10
Arthur Cohen <arthur.cohen@embecosm.com>
2024-06-02 19:17:38 +0200

    [wip]: more wip

--- typecheck/src/widen.rs
+++ typecheck/src/widen.rs
@@ -1,10 +1,30 @@
+use std::any::Any;
 use std::collections::HashSet;
 
-use crate::typemap::TypeMap;
 use crate::TypeCtx;
+use crate::{typemap::TypeMap, TypeSet};
 
 use fir::{Fir, RefIdx};
 use flatten::FlattenData;
+
+fn widen_inner(
+    fir: &Fir<FlattenData<'_>>,
+    type_ctx: TypeCtx<TypeMap>,
+    to_flatten: TypeSet,
+) -> TypeSet {
+
+
+    to_flatten.0.iter().fold(HashSet::new(), |typeset, entry| {
+        let to_flatten = type_ctx.types.type_of(entry.expect_resolved());
+
+        let set = match to_flatten {
+            Some(ty) => ty.set(),
+            None => unreachable!(),
+        };
+    })
+
+    todo!()
+}
 
 pub fn widen(fir: &Fir<FlattenData<'_>>, type_ctx: TypeCtx<TypeMap>) -> TypeCtx<TypeMap> {
     type_ctx
